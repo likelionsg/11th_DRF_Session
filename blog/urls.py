b0debbf548759a5383f1b82e8fd2e060836a0d0e
taeho0888 <taeho0888@naver.com>
2023-05-22 05:34:14 +0900

    urls 수정

--- blog/urls.py
+++ blog/urls.py
@@ -4,9 +4,9 @@ from .views import *
 urlpatterns = [
     # user urls
     path('user/', UserViewSet.as_view({'get':'list', 'post':'create'})),
-    path('user/<int:pk>/', UserViewSet.as_view({'get':'retrieve', 'put':'update','patch':'update','delete':'destroy'})),
+    path('user/<int:pk>/', UserViewSet.as_view({'get':'retrieve', 'put':'update','patch':'partial_update','delete':'destroy'})),
 
     # blog urls
     path('blog/', BlogViewSet.as_view({'get':'list', 'post':'create'})),
-    path('blog/<int:pk>/', BlogViewSet.as_view({'get':'retrieve', 'put':'update','patch':'update','delete':'destroy'})),
+    path('blog/<int:pk>/', BlogViewSet.as_view({'get':'retrieve', 'put':'update','patch':'partial_update','delete':'destroy'})),
 ]
