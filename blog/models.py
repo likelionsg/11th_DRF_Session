cfe47a357e52449c0052dc8456d4bf6613269c6b
taeho0888 <taeho0888@naver.com>
2023-05-22 04:58:44 +0900

    Blog 모델에 time 필드 추가

--- blog/models.py
+++ blog/models.py
@@ -12,6 +12,8 @@ class User(models.Model):
 class Blog(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog')
     
     def __str__(self):
